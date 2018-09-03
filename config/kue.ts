0f5ab045fb2109af07409d079c6480e8022323bd
Hamza Baig <hamzabaig18@gmail.com>
2018-09-03 13:52:25 +0500

    fixes issue with conditions when job gets stale

--- config/kue.ts
+++ config/kue.ts
@@ -30,41 +30,69 @@ async function evaluateConditions(jobData: any) {
   const actions: any[] = jobData.actions;
   const lastRunDate = jobData.last_run && jobData.last_run.date;
   let errored = null;
-  // If there is no last run date, then this is the first time they're being run
-  // Save the last run date and publish the actions with their params
+
+  const saveSuccessRunDate = async () => {
+    try {
+      await saveLastRun(jobData._id, jobData.timestamp);
+    } catch (e) {
+      logger.info(oneLine`
+        [Err] Error occurred while saving last_run info of Job: ${jobData._id}
+      `, e);
+    }
+  };
+
   try {
+    // If there is no last run date, then this is the first time they're being run
+    // Save the last run date and return
+
     if (!lastRunDate) {
       logger.info(oneLine`
-        [i] Didn't find any last_run_date for Job: ${jobData._id}, Running actions NOW
-        as warm up!
+        [i] Didn't find any last_run_date for Job: ${jobData._id}, Saving current date!
       `);
+      await saveSuccessRunDate();
+      return true;
+    }
+
+    // If there was a last_run_date and its diff with current date is FAR Greater than the
+    // saved condition (in mins) than save the last_date with current timestamp and return;
+    const lastDiffFromNowMins = Math.round((moment(lastRunDate).diff(moment()) / 1000) / 60);
+    const conditionArg = parseInt(conditions[0] ? <string> conditions[0].argument.value : '0');
+
+    if (lastDiffFromNowMins > (conditionArg + 1 )) {
+      logger.info(oneLine`
+        [i] Found last_run_date FAR Greater than current time for Job: ${jobData._id},
+        Saving current date!
+      `);
+      await saveSuccessRunDate();
+      return true;
+    }
+
+    // Else, evaluate the conditions
+    const timeDiff = moment(jobData.timestamp).diff(moment(lastRunDate));
+    const lastRunDiffInMins = Math.round((timeDiff / 1000) / 60);  // miliseconds to secs to mins
+    const evaluatedConditions = conditions.map((condition) => {
+      // Cron Periodical time will have only 1 condition and that'll be
+      // when: 'last_run_date_diff'
+      // operation: $eq
+      // argument: {
+      //  type: 'number'
+      //  value: X // value in minutes
+      // }
+      logger.info(`jobData.timestamp`);
+
+      return lastRunDiffInMins >= condition.argument.value;
+    });
+
+    // If all conditions are true, publish actions events
+    if (evaluatedConditions.reduce((error, cond) => error && cond, true)) {
+      logger.info(`[i] All Conditions true for Job: ${jobData._id}`);
+      logger.info(`[i] Publishing events for ${actions.length} actions with Job: ${jobData._id}`);
+      await saveSuccessRunDate();
       await publishActions(actions);
-    } else {
-      // Else, evaluate the conditions
-      const timeDiff = moment(jobData.timestamp).diff(moment(lastRunDate));
-      const lastRunDiffInMins = Math.round((timeDiff / 1000) / 60);  // miliseconds to secs to mins
-      const evaluatedConditions = conditions.map((condition) => {
-        // Cron Periodical time will have only 1 condition and that'll be
-        // when: 'last_run_date_diff'
-        // operation: $eq
-        // argument: {
-        //  type: 'number'
-        //  value: X // value in minutes
-        // }
-
-        return lastRunDiffInMins >= condition.argument.value;
-      });
-
-      // If all conditions are true, publish actions events
-      if (evaluatedConditions.reduce((error, cond) => error && cond, true)) {
-        logger.info(`[i] All Conditions true for Job: ${jobData._id}`);
-        logger.info(`[i] Publishing events for ${actions.length} actions with Job: ${jobData._id}`);
-        await publishActions(actions);
-        logger.info(oneLine`
-          [i] Publishing events for ${actions.length} actions was successfull
-          with Job: ${jobData._id}
-        `);
-      }
+      logger.info(oneLine`
+        [i] Published events for ${actions.length} actions was successfull
+        with Job: ${jobData._id}
+      `);
     }
   } catch (e) {
     logger.info(oneLine`
@@ -86,6 +114,7 @@ async function evaluateConditions(jobData: any) {
     // TODO: if error, add it to failed jobs queue
     throw errored;
   }
+
   return true;
 }
 
